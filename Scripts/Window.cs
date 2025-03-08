777409a525c00a7f8d0501e8014b633489f24ab1
slashie <slash@slashware.net>
2025-03-08 12:30:31 -0500

    Add OnHide delegate for window

--- Scripts/Window.cs
+++ Scripts/Window.cs
@@ -1,7 +1,16 @@
 using UnityEngine;
 
 public class Window: MonoBehaviour {
+    public delegate void OnHideDelegate();
+    public OnHideDelegate OnHide;
     public void Hide(){
         gameObject.SetActive(false);
+        if  (OnHide != null) {
+            OnHide();
+        }
+    }
+
+    public void Show(){
+        gameObject.SetActive(true);
     }
 }
